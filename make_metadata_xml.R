8bfd661f7f7ad92ef480404a856578a3a49a1812
alvizek <avizek@flowwest.com>
2025-01-09 16:27:19 -0800

    updates using new repo

--- make_metadata_xml.R
+++ make_metadata_xml.R
@@ -5,35 +5,36 @@ library(readxl)
 library(EML)
 
 datatable_metadata <-
-  dplyr::tibble(filepath = c("edi/data/catch.csv",
-                             "edi/data/recapture.csv",
-                             "edi/data/release.csv",
-                             "edi/data/trap.csv",
-                             "edi/data/environmental.csv"),
-                attribute_info = c("edi/metadata/catch_metadata.xlsx",
-                                   "edi/metadata/recapture_metadata.xlsx",
-                                   "edi/metadata/release_metadata.xlsx",
-                                   "edi/metadata/trap_visit_metadata.xlsx",
-                                   "edi/metadata/trap_visit_environmental_metadata.xlsx"),
+  dplyr::tibble(filepath = c("data/catch.csv",
+                             "data/recapture.csv",
+                             "data/release.csv",
+                             "data/trap.csv",
+                             "data/environmental.csv"),
+                attribute_info = c("metadata/catch_metadata.xlsx",
+                                   "metadata/recapture_metadata.xlsx",
+                                   "metadata/release_metadata.xlsx",
+                                   "metadata/trap_visit_metadata.xlsx",
+                                   "metadata/trap_visit_environmental_metadata.xlsx"),
                 datatable_description = c("Catch data from Deer and Mill creeks",
                                           "Recapture data from Deer and Mill creeks",
                                           "Release data from Deer and Mill creeks",
                                           "Trap visit data from Deer and Mill creeks",
                                           "Environmental data from Deer and Mill creeks"),
-                datatable_url = paste0("https://raw.githubusercontent.com/SRJPE/mill_deer_database_prep/main/data/",
-                                       c("deer_mill_catch_edi.csv",
-                                         "deer_mill_recapture_edi.csv",
-                                         "deer_mill_release_edi.csv",
-                                         "deer_mill_trap_edi.csv")))
+                datatable_url = paste0("https://raw.githubusercontent.com/SRJPE/jpe-deer-mill-datatackle-edi/main/data/",
+                                       c("catch.csv",
+                                         "recapture.csv",
+                                         "release.csv",
+                                         "trap.csv",
+                                         "environmental.csv")))
 
 # save cleaned data to `data/`
-excel_path <- "edi/metadata/deer_mill_metadata.xlsx" 
+excel_path <- "metadata/deer_mill_metadata.xlsx" 
 sheets <- readxl::excel_sheets(excel_path)
 metadata <- lapply(sheets, function(x) readxl::read_excel(excel_path, sheet = x))
 names(metadata) <- sheets
 
-abstract_docx <- "edi/metadata/abstract.docx"
-methods_docx <- "edi/metadata/methods.md"
+abstract_docx <- "metadata/abstract.docx"
+methods_docx <- "metadata/methods.md"
 
 #edi_number <- reserve_edi_id(user_id = Sys.getenv("EDI_USER_ID"), password = Sys.getenv("EDI_PASSWORD"))
 edi_number <- "edi.1775.1" 
@@ -69,15 +70,15 @@ eml <- list(packageId = edi_number,
             additionalMetadata = list(metadata = list(unitList = unitList))
 )
 edi_number
-EML::write_eml(eml, paste0("edi/",edi_number, ".xml"))
-EML::eml_validate(paste0("edi/",edi_number, ".xml"))
+EML::write_eml(eml, paste0(edi_number, ".xml"))
+EML::eml_validate(paste0(edi_number, ".xml"))
 
 # EMLaide::evaluate_edi_package(user_id = Sys.getenv("EDI_USER_ID"),
 #                                           password = Sys.getenv("EDI_PASSWORD"),
 #                                           eml_file_path = "edi/edi.1775.1.xml",
 #                                           environment = "staging")
 #
-EMLaide::upload_edi_package(user_id = Sys.getenv("EDI_USER_ID"),
-                            password = Sys.getenv("EDI_PASSWORD"),
-                            eml_file_path = "edi/edi.1775.1.xml",
-                            environment = "staging")
+# EMLaide::upload_edi_package(user_id = Sys.getenv("EDI_USER_ID"),
+#                             password = Sys.getenv("EDI_PASSWORD"),
+#                             eml_file_path = "edi.1775.1.xml",
+#                             environment = "production")
